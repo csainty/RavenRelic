bce85832738f1e0ebeb95d7f2640d1fd487fea0e
Chris Sainty <csainty@hotmail.com>
2012-03-29 13:02:22 +1100

    Added NewRelic tracking events to RavenDB requests.

--- RavenRelic/Monitor.cs
+++ RavenRelic/Monitor.cs
@@ -1,11 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Raven.Client.Connection;
+using Raven.Client.Connection.Profiling;
+using Raven.Client.Document;
 
 namespace RavenRelic
 {
     public class Monitor
     {
+        public static void AttachTo(DocumentStore store)
+        {
+            store.SessionCreatedInternal += TrackSession;
+            store.AfterDispose += AfterDispose;
+            if (store.JsonRequestFactory != null)
+            {
+                store.JsonRequestFactory.ConfigureRequest += BeginRequest;
+                store.JsonRequestFactory.LogRequest += EndRequest;
+            }
+        }
+
+        private static void TrackSession(InMemoryDocumentSessionOperations obj)
+        {
+            NewRelic.Api.Agent.NewRelic.IncrementCounter("RavenDB/SessionCount");
+        }
+
+        private static void BeginRequest(object sender, WebRequestEventArgs e)
+        {
+            NewRelic.Api.Agent.NewRelic.IncrementCounter("RavenDB/RequestCount");
+        }
+
+        private static void EndRequest(object sender, RequestResultArgs e)
+        {
+            string queryName = e.Url.Split('?')[0];
+            NewRelic.Api.Agent.NewRelic.RecordResponseTimeMetric("RavenDB/Query/" + queryName, (long)e.DurationMilliseconds);
+        }
+
+        private static void AfterDispose(object sender, EventArgs e)
+        {
+            var store = sender as DocumentStore;
+            if (store != null)
+            {
+                store.SessionCreatedInternal -= TrackSession;
+                store.AfterDispose -= AfterDispose;
+
+                if (store.JsonRequestFactory != null)
+                {
+                    store.JsonRequestFactory.ConfigureRequest -= BeginRequest;
+                    store.JsonRequestFactory.LogRequest -= EndRequest;
+                }
+            }
+        }
     }
 }
